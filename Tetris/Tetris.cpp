cb9e551822bc9be1ffe0043693d633921e163686
Jonas Fasterling <jonas@fasterling.de>
2021-01-15 22:39:52 +0100

    Spielfeldumrandung erstellt

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -4,7 +4,15 @@
 #include <iostream>
 using namespace std;
 
+#include <Windows.h>
+
 wstring tetromino[7];
+int iFieldWidth = 12;
+int iFieldHeight = 18;
+unsigned char* pField = nullptr;
+
+int iScreenWidth = 80;   //Höhe der Konsole
+int iScreenHeight = 30;  //Breite der Konsole
 
 int Rotate(int px, int py, int r)
 {
@@ -56,6 +64,50 @@ int main()
 	tetromino[6].append(L".XX.");
 	tetromino[6].append(L".X..");
 	tetromino[6].append(L".X..");
+
+	pField = new unsigned char[iFieldWidth * iFieldHeight]; //Erzeugen des Spielfelds
+	for (int x = 0; x < iFieldWidth; x++)
+	{
+		for (int y = 0; y < iFieldHeight; y++)
+		{
+			pField[y * iFieldWidth + x] = (x == 0 || x == iFieldWidth - 1 || y == iFieldHeight - 1) ? 9 : 0; //Spielfeldrand erzeugen
+		}
+	}
+
+	wchar_t* screen = new wchar_t[iScreenWidth * iScreenHeight];
+	for (int i = 0; i < iScreenWidth * iScreenHeight; i++)
+	{
+		screen[i] = L' ';
+	}
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+
+	bool bGameOver = false;
+
+	while (!bGameOver)
+	{
+		// Timing für alle Geraete
+
+
+		//Input des Spielers
+
+		//Logik des Spiels
+
+		//Output Rendern
+
+		//Spielfeld gestalten
+		for (int x = 0; x < iFieldWidth; x++)
+		{
+			for (int y = 0; y < iFieldHeight; y++)
+			{
+				screen[(y + 2) * iScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * iFieldWidth + x]];
+			}
+		}
+		//Frame darstellen
+		WriteConsoleOutputCharacter(hConsole, screen, iScreenWidth * iScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
 
 	return 0;
 }
